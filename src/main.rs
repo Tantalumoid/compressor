28ea0b08b8a28f3ea1f3142bcf5dd8f168c90af5
Tantalumoid <whisnowar@gmail.com>
2024-03-04 23:15:02 +0700

    unwtf

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use nanoid::nanoid;
-use std::{collections::HashMap, fmt::format, io::Write};
+use std::{collections::HashMap, io::Write};
 use unicode_segmentation::UnicodeSegmentation;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -10,13 +10,12 @@ fn main() {
         '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '`', ':', '~', '!', '@', '#', '$', '%', '^', '&',
-        '*', '(', ')', '-', '_', '=', '+', ';', '/', '?', '.', ',', '<', '>',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ':', '~', '!', '@', '#', '$', '%', '^', '&', '*',
+        '(', ')', '-', '_', '=', '+', ';', '/', '?', '.', ',', '<', '>', ' ', '\r', '\n', '\t',
     ];
     for word in words.iter() {
-        *codes.entry(word).or_insert(nanoid![1, &alphabet]) += &nanoid![1, &alphabet];
+        *codes.entry(word).or_insert(nanoid![2, &alphabet]) += "";
     }
-    println!("{:?}", codes);
     let mut vec_code_content = Vec::<String>::new();
     for word in words.iter() {
         codes.iter().for_each(|c| {
@@ -26,14 +25,13 @@ fn main() {
         });
     }
     let str_code_content = vec_code_content.into_iter().collect::<String>();
-    println!("{str_code_content}");
     let file_path = format!(
         "{}_compressed.{}",
         &args[1].rsplit_once('.').unwrap().0,
         &args[1].rsplit_once('.').unwrap().1
     );
-    println!("{file_path}");
     std::fs::File::create(file_path)
         .unwrap()
-        .write(str_code_content.as_bytes());
+        .write(str_code_content.as_bytes())
+        .unwrap();
 }
